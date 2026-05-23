026dfdb19123bcd0a90e3d468c61e1bb98d6e83b
biamat26 <bianchi1204@gmail.com>
2026-05-23 10:29:59 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/torneo_calcio/TorneoCalcioApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.torneo_calcio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorneoCalcioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorneoCalcioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/torneo_calcio/TorneoCalcioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.torneo_calcio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorneoCalcioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
